6a709837bed28726826d67da1a58bfbb81836bf5
zoheebishaq <zoheeb.ishaque@gmail.com>
2021-12-25 13:37:16 +0100

    commit

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss'],
 })
 export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!:FaceSnap
+
   title!: string;
   description!: string;
   createDate!: Date;
